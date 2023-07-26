fe0aae7d73a734871904a62ddb68b076d5800568
Marc Girbau <marc.girbau94@gmail.com>
2023-07-26 16:56:44 +0200

    Implement useMaterialTheme custom hook

---
+++ src/components/hooks/useMaterialTheme/useMaterialTheme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material";
+import { Theme, buildThemeOptions } from "../../../styles/buildThemeOptions";
+
+const useMaterialTheme = (theme: Theme) => {
+  const themeOptions = buildThemeOptions(theme);
+
+  return createTheme(themeOptions);
+};
+
+export default useMaterialTheme;
