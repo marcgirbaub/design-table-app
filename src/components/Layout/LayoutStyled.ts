b5cce5f962329f2e5cec87b44a1bfabfbfc26fcf
Marc Girbau <marc.girbau94@gmail.com>
2023-07-27 17:32:16 +0200

    Create Layout component and its styles

---
+++ src/components/Layout/LayoutStyled.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+const LayoutStyled = styled.div`
+  .main-content {
+    padding: 100px 60px 60px;
+
+    @media (max-width: 800px) {
+      padding: 20px;
+    }
+  }
+`;
+
+export default LayoutStyled;
