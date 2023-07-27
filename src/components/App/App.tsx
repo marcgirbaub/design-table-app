ad7e401b8ef78f0e963499be4d4cf4331edcc2ca
Marc Girbau <marc.girbau94@gmail.com>
2023-07-27 16:23:49 +0200

    Create Router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,15 @@
 import { ReactElement } from "react";
+import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const App = (): ReactElement => {
   const queryClient = new QueryClient();
 
-  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Outlet />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
---
+++ src/router/router.tsx
@@ -0,0 +1,11 @@
+import { RouteObject, createBrowserRouter } from "react-router-dom";
+import App from "../components/App/App";
+
+const routes: RouteObject[] = [
+  {
+    element: <App />,
+    children: [],
+  },
+];
+
+export const router = createBrowserRouter(routes);
