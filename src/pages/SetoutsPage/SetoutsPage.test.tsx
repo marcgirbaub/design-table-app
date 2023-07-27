1685227d581bc8df7390dc36515d0bcb863ed165
Marc Girbau <marc.girbau94@gmail.com>
2023-07-27 21:18:56 +0200

    Test data case in SetoutsPage component

--- src/pages/SetoutsPage/SetoutsPage.test.tsx
+++ src/pages/SetoutsPage/SetoutsPage.test.tsx
@@ -1,22 +1,44 @@
-import { screen } from "@testing-library/react";
-import renderWithProviders from "../../utils/renderWithProviders";
-
-import apiClient from "../../api/apiClient";
-import { designsMock } from "../../mocks/designsMocks";
+import { render, screen } from "@testing-library/react";
+import { useQuery } from "react-query";
 import SetoutsPage from "./SetoutsPage";
+import { setoutsMocks } from "../../mocks/setoutsMocks";
 
-jest.mock("../../api/apiClient");
+const mockUseQuery = useQuery as jest.Mock;
+
+jest.mock("react-query", () => ({
+  useQuery: jest.fn(),
+}));
 
 describe("Given a SetoutsPage component", () => {
   describe("When rendered and the data is being loaded", () => {
     test("Then it should show a loader", () => {
-      apiClient.get = jest.fn().mockResolvedValue({ data: designsMock });
+      mockUseQuery.mockReturnValue({ isLoading: true });
 
-      renderWithProviders(<SetoutsPage />);
+      render(<SetoutsPage />);
 
       const loaderElement = screen.getByRole("progressbar");
 
       expect(loaderElement).toBeInTheDocument();
+    });
+  });
+
+  describe("When rendered and the data is loaded", () => {
+    test("Then it should show a table with the title `Machine Name` and `Machine Width`", () => {
+      const nameTitle = "Machine Name";
+      const widthTitle = "Machine Width";
+
+      mockUseQuery.mockReturnValue({
+        isLoading: false,
+        data: { data: setoutsMocks },
+      });
+
+      render(<SetoutsPage />);
+
+      const machineNameTitleElement = screen.getByText(nameTitle);
+      const machineWidthTitleElement = screen.getByText(widthTitle);
+
+      expect(machineNameTitleElement).toBeInTheDocument();
+      expect(machineWidthTitleElement).toBeInTheDocument();
     });
   });
 });
