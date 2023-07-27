74396a7cf082b5bc954eaa8348d00b8c138d15c8
Marc Girbau <marc.girbau94@gmail.com>
2023-07-27 21:22:22 +0200

    Test when data is loaded in DesignsPage component

--- src/components/ThemeSwitch/ThemeSwitch.test.tsx
+++ src/components/ThemeSwitch/ThemeSwitch.test.tsx
@@ -1,13 +1,17 @@
-import { screen } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 import renderWithProviders from "../../utils/renderWithProviders";
 import ThemeSwitch from "./ThemeSwitch";
 
 describe("Given a ThemeSwitch component", () => {
-  test("Then should render", async () => {
-    renderWithProviders(<ThemeSwitch />);
+  describe("When rendered", () => {
+    test("Then it should show a switch with a test id equal to `dark-theme-switch`", () => {
+      renderWithProviders(<ThemeSwitch />);
 
-    const switchElement = screen.getByTestId("dark-theme-switch");
+      const switchElement = screen.getByTestId("dark-theme-switch");
 
-    expect(switchElement).toBeInTheDocument();
+      fireEvent.click(switchElement);
+
+      expect(switchElement).toBeInTheDocument();
+    });
   });
 });
--- src/pages/DesignsPage/DesignsPage.test.tsx
+++ src/pages/DesignsPage/DesignsPage.test.tsx
@@ -1,21 +1,44 @@
-import { screen } from "@testing-library/react";
-import renderWithProviders from "../../utils/renderWithProviders";
+import { render, screen } from "@testing-library/react";
+import { useQuery } from "react-query";
 import DesignsPage from "./DesignsPage";
-import apiClient from "../../api/apiClient";
 import { designsMock } from "../../mocks/designsMocks";
 
-jest.mock("../../api/apiClient");
+const mockUseQuery = useQuery as jest.Mock;
+
+jest.mock("react-query", () => ({
+  useQuery: jest.fn(),
+}));
 
 describe("Given a DesignsPage component", () => {
   describe("When rendered and the data is being loaded", () => {
     test("Then it should show a loader", () => {
-      apiClient.get = jest.fn().mockResolvedValue({ data: designsMock });
+      mockUseQuery.mockReturnValue({ isLoading: true });
 
-      renderWithProviders(<DesignsPage />);
+      render(<DesignsPage />);
 
       const loaderElement = screen.getByRole("progressbar");
 
       expect(loaderElement).toBeInTheDocument();
+    });
+  });
+
+  describe("When rendered and the data is loaded", () => {
+    test("Then it should show a table with the title `Wales` and `By`", () => {
+      const walesTitle = "Wales";
+      const byTitle = "By";
+
+      mockUseQuery.mockReturnValue({
+        isLoading: false,
+        data: { data: designsMock },
+      });
+
+      render(<DesignsPage />);
+
+      const walesTitleElement = screen.getByText(walesTitle);
+      const byTitleElement = screen.getByText(byTitle);
+
+      expect(walesTitleElement).toBeInTheDocument();
+      expect(byTitleElement).toBeInTheDocument();
     });
   });
 });
