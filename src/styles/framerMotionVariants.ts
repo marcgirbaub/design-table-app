dfec48ca640f69478f082c06075b78dc0d4ac9ac
Marc Girbau <marc.girbau94@gmail.com>
2023-07-27 23:51:31 +0200

    Create framer-motion variants

---
+++ src/styles/framerMotionVariants.ts
@@ -0,0 +1,29 @@
+import { Variants } from "framer-motion";
+
+export const downVariants: Variants = {
+  initial: {
+    opacity: 0,
+    y: 20,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
+
+export const leftVariants: Variants = {
+  initial: {
+    opacity: 0,
+    x: -20,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
