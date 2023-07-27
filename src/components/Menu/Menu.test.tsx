35456ceebfa26c272e470ce8169784cfef919bdc
Marc Girbau <marc.girbau94@gmail.com>
2023-07-27 19:24:18 +0200

    Refactor LeftSideMenu to Menu and add mediaqueries for mobiles

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -1,11 +1,11 @@
 import { screen } from "@testing-library/react";
 import renderRouterWithProviders from "../../utils/renderRouterWithProviders";
-import LeftSideMenu from "./LeftSideMenu";
+import Menu from "./Menu";
 
-describe("Given a LeftSideMenu component", () => {
+describe("Given a Menu component", () => {
   describe("When rendered", () => {
     test("Then it should show two links with the text `Designs` and `Setouts`", () => {
-      renderRouterWithProviders(<LeftSideMenu />);
+      renderRouterWithProviders(<Menu />);
 
       const designsLinkElement = screen.getByRole("link", { name: "Designs" });
       const setoutsLinkElement = screen.getByRole("link", { name: "Setouts" });
